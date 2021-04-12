3e1a45831e8ad37c7ba827d20d253044f02cb7f1
Javier Prats <javier.prats@frontify.com>
2021-04-12 16:04:36 +0200

    Add messenger.subscribeResponse test

--- src/messenger.test.ts
+++ src/messenger.test.ts
@@ -1,9 +1,11 @@
+import { GET_APP_STATE } from "./actions";
 import Messenger from "./Messenger";
 
 const originUrl = "https://www.localhost.com";
-const messenger = new Messenger(originUrl);
 const tokenLength = 6;
 const message = { key: "121243oas!6ihdf", token: "1719248ty$^&" };
+
+const messenger = new Messenger(originUrl);
 
 test("getMessageToken", () => {
     const token = messenger.getMessageToken();
@@ -15,4 +17,12 @@ test("postMessage", () => {
     messenger.postMessage(message);
     expect(window.top.postMessage).toHaveBeenCalledTimes(1);
     expect(window.top.postMessage).toHaveBeenCalledWith(message, originUrl);
+});
+
+test("subscribeResponse", () => {
+    window.addEventListener = jest.fn();
+    messenger.subscribeResponse(GET_APP_STATE, message.token);
+
+    expect(window.top.postMessage).toHaveBeenCalledTimes(1);
+    expect(window.addEventListener).toHaveBeenCalledWith("message", expect.any(Function), { once: true });
 });
