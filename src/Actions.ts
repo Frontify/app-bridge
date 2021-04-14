8716790a529a8cff9b15d9e9350da0ace3b9641c
Urs Rietberger <urs.rietberger@frontify.com>
2021-04-14 16:57:44 +0200

    Rename because the actual endpoint is a put

--- src/Actions.ts
+++ src/Actions.ts
@@ -1,13 +1,13 @@
 //Fetch keys
 export const GET_APP_STATE = "getAppState";
 export const GET_THIRDPARTY_OAUTH2_TOKEN = "getThirdpartyOAuth2Token";
-export const POST_APP_STATE = "postAppState";
+export const PUT_APP_STATE = "putAppState";
 export const POST_EXTERNAL_ASSET = "postExternalAsset";
 
 export type AllowedFetchKeys =
     | typeof GET_APP_STATE
     | typeof GET_THIRDPARTY_OAUTH2_TOKEN
-    | typeof POST_APP_STATE
+    | typeof PUT_APP_STATE
     | typeof POST_EXTERNAL_ASSET;
 
 //Dispatch keys
