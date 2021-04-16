d87b69c98fd90c6ac24c096d1bc6c9ee4aefe887
Urs Rietberger <urs.rietberger@frontify.com>
2021-04-16 13:50:27 +0200

    Remove key from the allowed fetch key because it should be only used within its own function

--- src/Actions.ts
+++ src/Actions.ts
@@ -1,14 +1,12 @@
 //Fetch keys
 export const GET_APP_STATE = "getAppState";
-export const GET_THIRDPARTY_OAUTH2_TOKEN = "getThirdpartyOAuth2Token";
 export const PUT_APP_STATE = "putAppState";
 export const POST_EXTERNAL_ASSET = "postExternalAsset";
 
-export type AllowedFetchKeys =
-    | typeof GET_APP_STATE
-    | typeof GET_THIRDPARTY_OAUTH2_TOKEN
-    | typeof PUT_APP_STATE
-    | typeof POST_EXTERNAL_ASSET;
+export type AllowedFetchKeys = typeof GET_APP_STATE | typeof PUT_APP_STATE | typeof POST_EXTERNAL_ASSET;
+
+//OAuth 2
+export const GET_THIRDPARTY_OAUTH2_TOKEN = "getThirdpartyOAuth2Token";
 
 //Dispatch keys
 export const DISPATCH_CLOSE_APP = "closeApp";
