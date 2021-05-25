61c1487e15a4863fbfc871b91b74bcf0b4df889a
urietberger <urietberger@users.noreply.github.com>
2021-05-25 10:06:42 +0000

    Released new version

---
+++ dist/Actions.d.ts
@@ -0,0 +1,10 @@
+export declare enum FetchKey {
+    GetAppState = "getAppState",
+    GetThirdPartyOauth2Token = "getThirdPartyOAuth2Token",
+    PutAppState = "putAppState",
+    ClearAppState = "clearAppState",
+    PostExternalAsset = "postExternalAsset"
+}
+export declare enum DispatchKey {
+    DispatchCloseApp = "closeApp"
+}
---
+++ dist/RequestType.d.ts
@@ -0,0 +1,4 @@
+export interface PostExternalAssetParams {
+    title: string;
+    url: string;
+}
