d9c897b8fb50fdee50412e2b179588bf0ababd83
urietberger <urietberger@users.noreply.github.com>
2021-05-25 08:56:25 +0000

    Released new version

--- dist/src/Actions.d.ts
+++ dist/src/Actions.d.ts
@@ -2,6 +2,7 @@ export declare enum FetchKey {
     GetAppState = "getAppState",
     GetThirdPartyOauth2Token = "getThirdPartyOAuth2Token",
     PutAppState = "putAppState",
+    ClearAppState = "clearAppState",
     PostExternalAsset = "postExternalAsset"
 }
 export declare enum DispatchKey {
--- dist/src/AppBridge.d.ts
+++ dist/src/AppBridge.d.ts
@@ -11,6 +11,7 @@ export default class AppBridge {
     getAppState<T>(): Promise<AppBridgeResponse<T>>;
     getThirdPartyOAuth2Token(): Promise<AppBridgeResponse<ThirdPartyOAuth2Token>>;
     putAppState<T>(state: T): Promise<AppBridgeResponse<T>>;
+    clearAppState(): Promise<AppBridgeResponse<null>>;
     postExternalAsset(asset: PostExternalAssetParams): Promise<AppBridgeResponse<Asset>>;
     private dispatch;
     private fetch;
