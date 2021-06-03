b21f463fe27babd646af6e4764da511f8d0cc8d3
urietberger <urietberger@users.noreply.github.com>
2021-06-03 08:54:18 +0000

    Released new version

--- dist/Messenger.d.ts
+++ dist/Messenger.d.ts
@@ -4,7 +4,7 @@ export interface CrossDocumentMessage<T = Record<string, unknown>> {
     token: string;
     data?: T;
 }
-export interface CrossDocumentMessageResponse<T> {
+export interface CrossDocumentMessageResponse<T = Record<string, unknown>> {
     success: boolean;
     key: DispatchKey | FetchKey;
     token: string;
