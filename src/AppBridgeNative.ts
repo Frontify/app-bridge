95183018b3b2aaeefe36bde7256d452653ca290a
Samuel Alev <samuel.alev@protonmail.com>
2021-07-01 11:22:55 +0200

    feat: add `getAssetById`, `updateBlockSettings`

--- src/AppBridgeNative.ts
+++ src/AppBridgeNative.ts
@@ -7,6 +7,7 @@ import type {
     AppBridgeUtilities,
 } from "./types/AppBridge";
 import { TerrificEvent } from "./types/TerrificEvent";
+import { getJqueryDataByElement, getJqueryDatasetByClassName } from "./utilities/jquery";
 
 export interface AppBridgeNative extends AppBridge {
     assets: AppBridgeAssets;
@@ -15,35 +16,83 @@ export interface AppBridgeNative extends AppBridge {
 }
 
 const assets: AppBridgeAssets = {
+    getAssetById(assetId: number): Promise<Asset> {
+        return new Promise(async (resolve) => {
+            const response = await window.fetch(`/api/asset/${assetId}`, {
+                method: "POST",
+                headers: {
+                    "x-csrf-token": (document.getElementsByName("x-csrf-token")[0] as HTMLMetaElement).content,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    settings: newSettings,
+                    ...(translationLanguage ? { language: translationLanguage } : {}),
+                }),
+            });
+
+            const responseJson = await response.json();
+            if (responseJson.success) {
+                return resolve(true);
+            } else {
+                throw new Error("Could not update the block settings");
+            }
+        });
+    },
+
     async postExternalAsset(asset: PostExternalAssetParams): Promise<Asset> {
         console.log(asset);
         return {} as Asset;
-    },
-
-    getAssetById(assetId: number): any {
-        return { foo: assetId };
     },
 };
 
 const block: AppBridgeBlock = {
     getBlockId(element: HTMLElement): number {
-        const parentDiv = element.closest("div[data-block]");
-        const parentDivBlockId = parseInt(parentDiv?.getAttribute("data-block") || "0");
+        const $parent = element.closest("div[data-block]");
+        const parentDivBlockId = parseInt($parent?.getAttribute("data-block") || "0");
 
-        if (parentDiv || parentDivBlockId > 0) {
+        if ($parent || parentDivBlockId > 0) {
             return parentDivBlockId;
         }
 
         throw new Error("Block's parent div not found.");
     },
 
-    getBlockSettings<T>(): Promise<T> {
+    getBlockSettings<T>(element: HTMLElement): Promise<T> {
+        console.log(element);
         return new Promise((r) => r({} as unknown as T));
     },
 
-    updateBlockSettings(newSettings: Record<string, unknown>): Promise<boolean> {
-        console.log(newSettings);
-        return new Promise((r) => r(true));
+    updateBlockSettings(element: HTMLElement, newSettings: Record<string, unknown>): Promise<boolean> {
+        const pageId = getJqueryDatasetByClassName("page").id;
+        if (!pageId) {
+            throw new Error("Page ID not found");
+        }
+
+        const sectionId = getJqueryDataByElement(element).id;
+        const blockId = this.getBlockId(element);
+
+        const { translationLanguage } = getJqueryDataByElement(document.body);
+
+        return new Promise(async (resolve) => {
+            const response = await window.fetch(`/api/document/block/${pageId}/${sectionId}/${blockId}`, {
+                method: "POST",
+                headers: {
+                    "x-csrf-token": (document.getElementsByName("x-csrf-token")[0] as HTMLMetaElement).content,
+                    "Content-Type": "application/json",
+                },
+                body: JSON.stringify({
+                    settings: newSettings,
+                    ...(translationLanguage ? { language: translationLanguage } : {}),
+                }),
+            });
+
+            const responseJson = await response.json();
+            if (responseJson.success) {
+                return resolve(true);
+            } else {
+                throw new Error("Could not update the block settings");
+            }
+        });
     },
 };
 
