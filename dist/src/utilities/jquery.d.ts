d03a64d11dfd0e4f64bd0e8c0edfdd5ad86c7d33
SamuelAlev <SamuelAlev@users.noreply.github.com>
2021-07-02 09:32:01 +0000

    Released new version

---
+++ dist/src/utilities/jquery.d.ts
@@ -0,0 +1,2 @@
+export declare const getJqueryDataByElement: (element: Element) => Record<string, unknown>;
+export declare const getJqueryDatasetByClassName: (className: string) => Record<string, unknown>;
