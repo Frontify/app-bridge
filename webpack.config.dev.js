207277166db401e0a906d5d7fabaadbe2f521f5e
Samuel Alev <samuel.alev@protonmail.com>
2021-10-06 14:33:55 +0200

    feat: better dev sourcemap

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -1,6 +1,6 @@
 //eslint-disable-next-line
 const baseConfig = require("./webpack.config.base");
 
-const config = { mode: "development", devtool: "eval", watch: true };
+const config = { mode: "development", devtool: "eval-source-map", watch: true };
 
 module.exports = { ...baseConfig, ...config };
