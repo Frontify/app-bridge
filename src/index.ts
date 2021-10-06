906e1b0676c0ca076cbe5cf28e4aa14405919802
Samuel Alev <samuel.alev@protonmail.com>
2021-10-06 14:34:22 +0200

    refactor: use export directly

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,8 @@
 import appBridgeIframe from "./AppBridgeIframe";
-import { AppBridgeNative } from "./AppBridgeNative";
+export { AppBridgeNative } from "./AppBridgeNative";
 import { AppBridgeIframe } from "./types";
 
 export * from "./types";
-
-export const appBridgeNative = AppBridgeNative;
 
 export const createIframeAppBridge = (): AppBridgeIframe => {
     return appBridgeIframe;
