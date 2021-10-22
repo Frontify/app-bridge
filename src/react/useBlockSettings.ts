87d48388bcfd76799add3d34f30529109e0ba2c6
Samuel Alev <samuel.alev@protonmail.com>
2021-10-22 13:09:33 +0200

    fix: correct issue with reactivity of the object

--- src/react/useBlockSettings.ts
+++ src/react/useBlockSettings.ts
@@ -8,25 +8,23 @@ export const useBlockSettings = <T = Record<string, unknown>>(
         throw new Error("You need to instanciate the App Bridge with a block id.");
     }
 
-    const [blockSettings, setBlockSettings] = useState<T>(window.blockSettings[appBridge.blockId] as T);
-
     if (!window.blockSettings[appBridge.blockId].__isProxy) {
         window.blockSettings[appBridge.blockId] = new Proxy(window.blockSettings[appBridge.blockId], {
-            get: (target: Record<string, unknown>, property: string, receiver: unknown) => {
+            get: (target: Record<string, unknown>, property: string, received: unknown) => {
                 if (property === "__isProxy") {
                     return true;
-                } else if (property) {
-                    return target[property];
                 }
-                return Reflect.get(target, property, receiver);
+                return Reflect.get(target, property, received);
             },
             set: (target: Record<string, unknown>, property: string, value: unknown) => {
+                setBlockSettings({ ...(target as T), [property]: value });
                 target[property] = value;
-                setBlockSettings({ ...(target as T) });
                 return true;
             },
         });
     }
+
+    const [blockSettings, setBlockSettings] = useState<T>(window.blockSettings[appBridge.blockId] as T);
 
     const setBlockSettingsAndUpdate = async (newSettings: T): Promise<void> => {
         if (appBridge.blockId === undefined) {
