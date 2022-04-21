02c4faab4dba811503a6553c425cfa887f4faf1d
Bojan Mazej <517677+bojangles-m@users.noreply.github.com>
2022-04-21 16:33:44 +0200

    test: add app bridge mock paramter

--- src/react/useAssetChooser.ts
+++ src/react/useAssetChooser.ts
@@ -2,13 +2,14 @@
 
 import { AssetChooserAssetChosenCallback, AssetChooserOptions } from '../types/Terrific';
 import { AppBridgeNative } from '../AppBridgeNative';
+import { IAppBridgeNative } from '../IAppBridgeNative';
 
 type UseAssetChooserType = {
     openAssetChooser: (callback: AssetChooserAssetChosenCallback, options: AssetChooserOptions) => void;
     closeAssetChooser: () => void;
 };
 
-export const useAssetChooser = (): UseAssetChooserType => {
-    const { openAssetChooser, closeAssetChooser } = new AppBridgeNative();
+export const useAssetChooser = (appBridge?: IAppBridgeNative): UseAssetChooserType => {
+    const { openAssetChooser, closeAssetChooser } = appBridge ?? new AppBridgeNative(0, 0);
     return { openAssetChooser, closeAssetChooser };
 };
