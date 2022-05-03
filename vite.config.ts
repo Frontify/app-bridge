49f0350b97c08d0381f9e394fb01c3ef58eba072
Samuel Alev <samuel.alev@frontify.com>
2022-05-03 11:16:57 +0200

    chore: prettier

--- vite.config.ts
+++ vite.config.ts
@@ -1,25 +1,25 @@
 /* (c) Copyright Frontify Ltd., all rights reserved. */
 
-import { defineConfig } from "vite";
-import { peerDependencies as peerDependenciesMap } from "./package.json";
-import { resolve } from "path";
-import dts from "vite-plugin-dts";
+import { defineConfig } from 'vite';
+import { peerDependencies as peerDependenciesMap } from './package.json';
+import { resolve } from 'path';
+import dts from 'vite-plugin-dts';
 
 const peerDependencies = Object.keys(peerDependenciesMap);
 
 export const globals = {
-    react: "React",
-    "react-dom": "ReactDOM",
+    react: 'React',
+    'react-dom': 'ReactDOM',
 };
 
 export default defineConfig({
     plugins: [dts({ insertTypesEntry: true })],
     build: {
         lib: {
-            entry: resolve(__dirname, "src/index.ts"),
+            entry: resolve(__dirname, 'src/index.ts'),
             fileName: (format: string) => `index.${format}.js`,
-            formats: ["es", "umd", "cjs"],
-            name: "AppBridge",
+            formats: ['es', 'umd', 'cjs'],
+            name: 'AppBridge',
         },
         sourcemap: true,
         minify: true,
