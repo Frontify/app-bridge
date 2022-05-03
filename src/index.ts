9e20d75cc9622b2b8ea6690ae129ca417782cdd3
Samuel Alev <samuel.alev@frontify.com>
2022-05-03 14:56:40 +0200

    chore: remove double export

--- src/index.ts
+++ src/index.ts
@@ -2,8 +2,6 @@
 
 export * from './AppBridgeIframe';
 export * from './AppBridgeNative';
-export * from './tests/AppBridgeNativeDummy';
-export * from './types/IAppBridgeNative';
 export * from './react';
 export * from './tests';
 export * from './types';
