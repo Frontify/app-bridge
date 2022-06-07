abe128249e1560eed9566188164fbba9c8e8f197
Samuel Alev <samuel.alev@frontify.com>
2022-06-07 14:30:27 +0200

    test: add test cases

--- src/utilities/object.spec.ts
+++ src/utilities/object.spec.ts
@@ -5,6 +5,14 @@ import { compareObjects } from './object';
 
 describe('compareObjects', () => {
     test.each([
+        [
+            { a: 1, b: 2 },
+            { a: 1, b: 2 },
+        ],
+        [
+            { b: 2, a: 1 },
+            { a: 1, b: 2 },
+        ],
         [{ a: { b: 2 } }, { a: { b: 2 } }],
         [
             { a: { b: 2 }, c: 3 },
@@ -18,28 +26,28 @@ describe('compareObjects', () => {
         expect(compareObjects(obj1, obj2)).toBeTruthy();
     });
 
-    // test.each([
-    //     [
-    //         { a: 1, b: 0, c: 3 },
-    //         { a: 1, b: 2, c: 3 },
-    //     ],
-    //     [
-    //         { a: 1, b: 2, c: 3 },
-    //         { a: 1, b: 2, c: 4 },
-    //     ],
-    //     [
-    //         { a: 1, b: 2, c: 3 },
-    //         { a: 1, b: 2, c: 3, d: 4 },
-    //     ],
-    //     [
-    //         { a: 1, b: 2, c: 3, d: 4, e: 5 },
-    //         { a: 1, b: 2, c: 3 },
-    //     ],
-    //     [
-    //         { a: 1, b: 2, c: 3 },
-    //         { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6 },
-    //     ],
-    // ])('should return false if objects are not equal', (obj1, obj2) => {
-    //     expect(compareObjects(obj1, obj2)).toBeFalsy();
-    // });
+    test.each([
+        [
+            { a: 1, b: 0, c: 3 },
+            { a: 1, b: 2, c: 3 },
+        ],
+        [
+            { a: 1, b: 2, c: 3 },
+            { a: 1, b: 2, c: 4 },
+        ],
+        [
+            { a: 1, b: 2, c: 3 },
+            { a: 1, b: 2, c: 3, d: 4 },
+        ],
+        [
+            { a: 1, b: 2, c: 3, d: 4, e: 5 },
+            { a: 1, b: 2, c: 3 },
+        ],
+        [
+            { a: 1, b: 2, c: 3 },
+            { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6 },
+        ],
+    ])('should return false if objects are not equal', (obj1, obj2) => {
+        expect(compareObjects(obj1, obj2)).toBeFalsy();
+    });
 });
