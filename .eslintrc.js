002df5235778b11b48e1b1850db1ee2ad80f5ba9
Goran Ribic <goran.ribic@frontify.com>
2022-07-12 15:47:23 +0200

    add: tsdocs packages

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,8 +1,12 @@
 module.exports = {
     extends: ['@frontify/eslint-config-react'],
+    plugins: ['eslint-plugin-tsdoc'],
     settings: {
         react: {
             version: 'detect',
         },
+    },
+    rules: {
+        'tsdoc/syntax': 'warn',
     },
 };
